0b7be733516b87e725caa2aabed5c7b6923b1331
Shamanth Shetty <122071009+Shamanth1252@users.noreply.github.com>
2024-10-30 10:11:31 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -2,8 +2,8 @@ def generate_pubic_key(prime,base,private_key):
     return pow(base,private_key,prime)
 def generate_shared_secret(public_key,private_key,prime):
     return pow(public_key,private_key,prime)
-prime=int(input("enter the large prime number(recommanded>2048 bits)"));
-base=int(input("enter the large prime number(recommanded>2048 bits)"));
+prime=int(input("enter the large prime number(recommanded>2048 bits)"))
+base=int(input("enter the large prime number(recommanded>2048 bits)"))
 
 aliceprivate=int(input("aliceenter your private key(a random interger less than the prime)"))
 alice_public=generate_public_key(prime,base,alice_private)
@@ -18,4 +18,11 @@ private("bobs publickey:",bob_public)
 alice_shared_secret=generate_shared_secret(bob_public,aliceprivate,prime)
 bob_shared_secret=generate_shared_secret(alice_public,bobprivate,prime)
 
-print()
+print("Alice Shared secrte:",alice_shared_secret)
+print("Bob shared secret",bob_shared_secret)
+
+if alice_shared_secret== bob_shared_secret:
+    print("Sucess the secret key is",alice_shared_secret)
+else:
+    print("Error :shared secret do not matched")
+
