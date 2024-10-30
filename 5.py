9733e90f733d37c53a3288c652fbf26f69d26bcf
Shamanth Shetty <122071009+Shamanth1252@users.noreply.github.com>
2024-10-30 10:06:21 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,21 @@
+def generate_pubic_key(prime,base,private_key):
+    return pow(base,private_key,prime)
+def generate_shared_secret(public_key,private_key,prime):
+    return pow(public_key,private_key,prime)
+prime=int(input("enter the large prime number(recommanded>2048 bits)"));
+base=int(input("enter the large prime number(recommanded>2048 bits)"));
+
+aliceprivate=int(input("aliceenter your private key(a random interger less than the prime)"))
+alice_public=generate_public_key(prime,base,alice_private)
+print("Alice public key %d",alice_public)
+
+bobprivate=int(input("aliceenter your private key(a random interger less than the prime)"))
+bob_public=generate_public_key(prime,base,alice_private)
+print("Alice public key %d",alice_public)
+
+private("bobs publickey:",bob_public)
+
+alice_shared_secret=generate_shared_secret(bob_public,aliceprivate,prime)
+bob_shared_secret=generate_shared_secret(alice_public,bobprivate,prime)
+
+print()
